42c4542e5e0435527959def8d98bd1c83bec25af
madhav <eragon.cube@outlook.com>
2025-03-01 23:59:54 +0530

    Setup shadcn in tailwind v4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkLoaded, ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          <ClerkLoaded>{children}</ClerkLoaded>
+          {children}
         </body>
       </html>
     </ClerkProvider>
